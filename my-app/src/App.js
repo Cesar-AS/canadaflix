4942f7c9b7b16ffb0eb1c778a624cfc3817b0197
Cesar Silva <csilvaatc@gmail.com>
2020-07-27 19:22:12 -0300

    Correção de Texto no primeiro video

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,9 +11,9 @@ function App() {
       <Menu />
 
     <BannerMain 
-      videoTitle={dadosIniciais.categorias[0].videos[0].titulo[0]}
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-end? Trabalhando na área"}
+      videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazenm afinal? Descubra com a Vanessa!"}
     />
 
     <Carousel 
