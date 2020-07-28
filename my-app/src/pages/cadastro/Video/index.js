22617ed490f4793d322dc2c13f88b9ec250a5bd8
Cesar Silva <csilvaatc@gmail.com>
2020-07-28 11:20:51 -0300

    Rotas, Carrossel atualizado e Páginas de Cadastro adicionadas

---
+++ my-app/src/pages/cadastro/Video/index.js
@@ -0,0 +1,17 @@
+import React from './node_modules/react';
+import PageDefault from '../../../components/PageDefault';
+import { Link } from './node_modules/react-router-dom';
+ 
+function CadastroVideo() {
+    return (
+      <PageDefault>
+        <h1>Cadastro de Vídeo</h1>
+
+        <Link to="/cadastro/categoria">
+            Cadastrar Categoria
+        </Link>
+      </PageDefault>
+    )
+}
+
+export default CadastroVideo;
